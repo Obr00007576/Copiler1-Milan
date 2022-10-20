a04c6d137dbc5a2a082847069d1509591bd9fae1
Obr00007576 <51303014+Obr00007576@users.noreply.github.com>
2022-10-21 03:41:07 +0800

    2

--- parser.py
+++ parser.py
@@ -6,13 +6,11 @@ const_tab = {}
 const_num = 0
 mem_tab = {}
 P_CODE=[]
-p_line=0
 
 def p_program(p):
     'program : BEGIN statementList END'
-    global p_line,P_CODE
+    global P_CODE
     p[0]=p[2]+['STOP']
-    p_line+=1
     P_CODE+=p[0]
     print(len(p[0]))
 
@@ -36,37 +34,30 @@ def p_statement(p):
                  | WHILE relation DO statementList OD
                  | WRITE LPAREN expression RPAREN
     '''
-    global mem_tab,p_line
+    global mem_tab
     p[0]=[]
     if p[2]==':=':
         mem_address = mem_tab[p[1]][1]
         p[0]=p[3]+[f"STORE {mem_address}"]
-        p_line+=1
     if p[1]=='write':
         p[0]=p[3]+['OUT']
-        p_line+=1
     if p[1]=='if':
         if len(p)==6:
-            p_line+=1
             p[0]=p[2]+[f'JMP_NO {len(p[4])+1}']+p[4]
         elif len(p)==8:
-            p_line+=2
             p[0]=p[2]+[f'JMP_NO {len(p[4])+2}']+p[4]+[f'JMP {len(p[6])+1}']+p[6]
     if p[1]=='while':
-        p[0]=p[2]+[f"JMP_NO {len(p[4])+2}"]+p[4]+[f"JMP {-len(p[2])-len(p[4])}"]
+        p[0]=p[2]+[f"JMP_NO {len(p[4])+2}"]+p[4]+[f"JMP {-1-len(p[2])-len(p[4])}"]
 
 def p_expression(p):
     '''expression : term
                   | expression ADDOP term
     '''
-    global p_line
     if len(p)==4:
         if p[2]=='A_PLUS':
             p[0]=p[1]+p[3]+['ADD']
-            p_line+=1
         elif p[2]=='A_MINUS':
             p[0]=p[1]+p[3]+['SUB']
-            p_line+=1
     else:
         p[0]=p[1]
 
@@ -74,14 +65,11 @@ def p_term(p):
     '''term : factor
             | factor MULOP factor
     '''
-    global p_line
     if len(p)==4:
         if p[2]=='A_MULTIPLY':
             p[0]=p[1]+p[3]+['MULT']
-            p_line+=1
         elif p[2]=='A_DIVIDE':
             p[0]=p[1]+p[3]+['DIV']
-            p_line+=1
     else:
         p[0]=p[1]
 
@@ -91,24 +79,28 @@ def p_factor(p):
               | READ
               | LPAREN expression RPAREN
     '''
-    global mem_tab,p_line
+    global mem_tab
     if len(p)==2:
         if p[1]=='read':
             p[0]=['INPUT']
-            p_line+=1
         else:
             mem_address = mem_tab[p[1]][1]
             p[0]=[f"LOAD {mem_address}"]
-            p_line+=1
     else:
         p[0]=p[2]
 
 def p_relation(p):
     'relation : expression CMP expression'
-    global p_line
-    global mem_tab
-    p[0]=p[1]+p[3]+['CMP']
-    p_line+=1
+    if p[2]=='C_EQ':
+        p[0]=p[1]+p[3]+['CMP 0']
+    elif p[2]=='C_LT':
+        p[0]=p[1]+p[3]+['CMP 1']
+    elif p[2]=='C_LE':
+        p[0]=p[1]+p[3]+['CMP 2']
+    elif p[2]=='C_GT':
+        p[0]=p[1]+p[3]+['CMP 3']
+    elif p[2]=='C_GE':
+        p[0]=p[1]+p[3]+['CMP 4']
 
 parser = yacc.yacc(debug=True)
 
@@ -153,7 +145,6 @@ if __name__=='__main__':
         mem_tab[key] = (var_tab[key][0], var_tab[key][1]+len(const_tab))
     print(mem_tab)
     parser.parse(data, lexer=lexer)
-    print(p_line)
     i=0
     for p in P_CODE:
         print(f"{i}: {p}")
